707553e12fd060c3967cd117e798fa51c2bcab29
= <=>
2025-08-20 11:21:44 -0500

    updateing

--- Taller1/punto1.kt
+++ Taller1/punto1.kt
@@ -1,14 +1,30 @@
+import kotlin.math.pow
+
 fun main() {
-    println(decimalToBinary(13, ""))
+    println(decimalToBinary(10233, ""))
+    println(binaryToDecimal("10011111111001",0))
 }
 fun decimalToBinary(aware: Int,binary: String): String{
     var awareFun = aware
+    var binaryFun = binary
     var residue = awareFun % 2
     awareFun = aware / 2
 
-    if(aware == 0){
+    if(awareFun == 0){
         return "$residue$binary"
     }else{
-        return "${decimalToBinary(awareFun,binary)}$binary"
+        return "${decimalToBinary(awareFun,binary)}$residue$binary"
     }
+}
+
+fun binaryToDecimal (binary : String,decimal : Int):Int{
+    if(binary.length == 1){
+        var binaryNumber = binary.toInt()
+        return binaryNumber + decimal
+    }
+    
+    var currentDecimal = 2.0.pow(binary.length - 1).toInt()
+    currentDecimal = currentDecimal * (binary[0].digitToInt())
+    var currentBinary = binary.drop(1)
+    return binaryToDecimal(currentBinary , currentDecimal + decimal)
 }
